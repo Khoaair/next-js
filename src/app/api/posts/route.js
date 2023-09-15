65cd54b2e54142e733ab4ed482dab6fccf4cae0c
KhoaAIr <KhoaAir@gmail.com>
2023-09-15 16:13:11 +0700

    dashboard page

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -14,3 +14,16 @@ export const GET = async request => {
     return new NextResponse('Database Err', { status: 500 });
   }
 };
+
+export const POST = async request => {
+  const body = await request.json();
+  const newPost = new Post(body);
+
+  try {
+    await connect();
+    await newPost.save();
+    return new NextResponse('Post has been created', { status: 201 });
+  } catch (error) {
+    return new NextResponse('Database Err', { status: 500 });
+  }
+};
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,10 @@
 'use client';
 import { useEffect, useState } from 'react';
-import style from './page.module.css';
+import styles from './page.module.css';
 import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -37,6 +38,30 @@ const Dashboard = () => {
     fetcher
   );
   console.log(data);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const image = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+      await fetch('/api/posts/', {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          desc,
+          image,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (session.status === 'loading') {
     return <h1>Loading...</h1>;
   }
@@ -44,7 +69,54 @@ const Dashboard = () => {
     router.push('/dashboard/login');
   }
   if (session.status === 'authenticated') {
-    return <div className={style.dashboard}>Dashboard</div>;
+    return (
+      <div className={styles.dashboard}>
+        <div className={styles.dashboard__posts}>
+          {isLoading
+            ? 'Is Loading'
+            : data.map(post => (
+                <div className={styles.dashboard__post} key={post._id}>
+                  <div className={styles.post__image}>
+                    <Image
+                      src={post.image}
+                      alt=''
+                      width={200}
+                      height={100}
+                      className={styles.img}
+                    />
+                  </div>
+                  <h2 className={styles.post__title}>{post.title}</h2>
+                  <span className={styles.post__delete}>x</span>
+                </div>
+              ))}
+        </div>
+        <form className={styles.dashboard__form} onSubmit={handleSubmit}>
+          <h1>Add new post</h1>
+          <input
+            type='text'
+            className={styles.form__input}
+            placeholder='Title'
+          />
+          <input
+            type='text'
+            className={styles.form__input}
+            placeholder='Description'
+          />
+          <input
+            type='text'
+            className={styles.form__input}
+            placeholder='Image'
+          />
+          <textarea
+            cols='30'
+            rows='10'
+            className={styles.form__textarea}
+            placeholder='Content'
+          ></textarea>
+          <button className={styles.form__button}>send</button>
+        </form>
+      </div>
+    );
   }
 };
 export default Dashboard;
